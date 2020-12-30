149edf82f0e26e03aa145a7716181e93a9df8d34
pessin <carlospessin@gmail.com>
2020-12-30 11:18:54 -0300

    Added DB Config

--- db.php
+++ db.php
@@ -1,5 +1,21 @@
 <?php
-$db = mysqli_connect("localhost", "root", "", "todo-list");
+
+// $db_host = 'localhost';
+// $db_user = 'root';
+// $db_pass = '';
+// $db_name = 'todo-list';
+
+$db_host = 'us-cdbr-east-02.cleardb.com';
+$db_user = 'be54de530cabb2';
+$db_pass = 'a2170668';
+$db_name = 'heroku_befb7e2cd27cbb2';
+
+$db = 
+mysqli_connect(
+  $db_host, 
+  $db_user, 
+  $db_pass, 
+  $db_name);
 
 if (!$db) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
